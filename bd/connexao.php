a24166890d9c479c1e565c2676d7fbc906c5cab7
Hugo Guandaline <vhguandaline@gmail.com>
2012-04-15 21:54:27 -0700

    Classe de conexao criada

--- bd/connexao.php
+++ bd/connexao.php
@@ -1,18 +1,80 @@
 <?php
 
 class connexao {
+
     private $banco;
-    private $senha;
+    private $user;
+    private $pass;
     private $conexao;
-    
-    function __construct($banco, $senha=NULL) {
+    private $resultado;
+    private $obj = NULL;
+    private $array = NULL;
+
+    function __construct($host, $banco, $user, $pass = NULL) {
         $this->banco = $banco;
-        $this->senha = $senha;
-        $this->conexao = new PDO('mysql:host=localhost;port=3306;dbname='.$banco,'root'); // conecta o servidor
+        $this->pass = $pass;
+        $this->host = $host;
+        $this->user = $user;
+        try {
+            if ($pass != NULL)
+                $this->conexao = new PDO('mysql:host=' . $this->host . ';port=3306;dbname=' . $this->banco, $this->user); // conecta o servidor
+            else
+                $this->conexao = new PDO('mysql:host=' . $this->host . ';port=3306;dbname=' . $this->banco, $this->user, $this->pass); // conecta o servidor
+            echo 'Conexão estabelecida!';
+        } catch (PDOException $e) {
+            echo 'error: falha ao criar conexão\n'.$e->getMessage();
+        }
+    }
+
+    public function query($sql) {
+        try{
+            $this->resultado = $this->conexao->prepare($sql);
+        }catch(PDOException $e){
+            echo 'error: function query\n'.$e->getMessage();
+        }
     }
     
-    public function query($sql){
-        
+    public function execute($dados = NULL){
+        try{
+            $this->resultado->execute($dados);
+        }  catch (PDOException $e){
+            echo 'error: function execute\n'.$e->getMessage();
+        }
+    }
+    
+    public function fetch(){
+        try{
+            $this->obj = $this->resultado->fetch(PDO::FETCH_OBJ);
+        }  catch (PDOException $e){
+            echo 'error: function fetch\n'.$e->getMessage();
+            $this->obj = NULL;
+        }
+        return $this->obj;
+    }
+
+    public function fetchAll(){
+        try{
+            $this->array = $this->resultado->fetchAll();
+        }  catch (PDOException $e){
+            echo 'error: function fetchAll\n'.$e->getMessage();
+            $this->array = NULL;
+        }
+        return $this->array;
+    }
+    
+    public function queryAll($sql, $dados){
+        try{
+            query($sql);
+            execute($dados);
+            return fetchAll();
+        }catch(PDOException $e){
+            echo 'error: function queryAll'.$e->getMessage();
+            return NULL;
+        }
+    }
+
+    public function close(){
+        $this->conexao = NULL;
     }
 
 }
