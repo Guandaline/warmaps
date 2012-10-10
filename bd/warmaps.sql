73181e5ab86d328a939d63cc98508c53ea0057e1
Hugo Guandaline <vhguandaline@gmail.com>
2012-10-10 12:35:38 -0400

    Nova estrutura do banco de dados

--- bd/warmaps.sql
+++ bd/warmaps.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 28/09/2012 às 00h28min
+-- Tempo de Geração: 10/10/2012 às 12h33min
 -- Versão do Servidor: 5.1.63
 -- Versão do PHP: 5.3.6-13ubuntu3.9
 
@@ -17,36 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Banco de Dados: `wm2`
+-- Banco de Dados: `warmaps`
 --
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cartas`
---
-
-CREATE TABLE IF NOT EXISTS `cartas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_mapa` int(11) NOT NULL,
-  `id_territorio` int(11) NOT NULL,
-  `valor` int(11) NOT NULL,
-  `figura` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `conquista_regioes`
---
-
-CREATE TABLE IF NOT EXISTS `conquista_regioes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_obj_continentes` int(11) NOT NULL,
-  `id_continente` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -138,7 +110,14 @@ CREATE TABLE IF NOT EXISTS `mapas` (
   `numero_objetivos` int(11) DEFAULT NULL,
   `tipo` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=84 ;
+
+--
+-- Extraindo dados da tabela `mapas`
+--
+
+INSERT INTO `mapas` (`id`, `nome`, `mapa`, `numero_territorios`, `numero_regioes`, `numero_objetivos`, `tipo`) VALUES
+(83, 'Mapa_final.svg', 'Mapa', 43, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -149,52 +128,16 @@ CREATE TABLE IF NOT EXISTS `mapas` (
 CREATE TABLE IF NOT EXISTS `objetivos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_mapa` int(11) NOT NULL,
-  `tipo` int(11) NOT NULL,
+  `tipo` varchar(30) NOT NULL,
   `nome` varchar(20) DEFAULT NULL,
-  `descricao` varchar(250) NOT NULL,
+  `conquistarcontinente` float NOT NULL,
+  `conquistafacil` float NOT NULL,
+  `tomarcontinente` varchar(11) NOT NULL,
+  `reg1` varchar(40) NOT NULL,
+  `reg2` varchar(40) NOT NULL,
+  `outro` varchar(80) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `obj_inimigo`
---
-
-CREATE TABLE IF NOT EXISTS `obj_inimigo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_obj` int(11) NOT NULL,
-  `cor` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `obj_regioes`
---
-
-CREATE TABLE IF NOT EXISTS `obj_regioes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_objetivo` int(11) NOT NULL,
-  `numero_continentes` int(11) NOT NULL,
-  `qualquer` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `obj_territorios`
---
-
-CREATE TABLE IF NOT EXISTS `obj_territorios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_objetivo` int(11) NOT NULL,
-  `numero_territorios` int(11) NOT NULL,
-  `numero_exercitos` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -226,7 +169,19 @@ CREATE TABLE IF NOT EXISTS `regioes` (
   `exercitos` int(11) DEFAULT NULL,
   `valor_estrategico` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+--
+-- Extraindo dados da tabela `regioes`
+--
+
+INSERT INTO `regioes` (`id`, `id_mapa`, `nome`, `cor`, `exercitos`, `valor_estrategico`) VALUES
+(27, 83, 'America do Sul', 'amarelo', 4, 4),
+(28, 83, 'America do Norte', 'verde', 7, 7),
+(29, 83, 'Europa', 'vermelho', 8, 8),
+(30, 83, 'Africa', 'azul', 5, 5),
+(31, 83, 'Asia', 'branco', 9, 9),
+(32, 83, 'Oceania', 'preto', 4, 4);
 
 -- --------------------------------------------------------
 
@@ -258,157 +213,8 @@ CREATE TABLE IF NOT EXISTS `territorios` (
   `label` varchar(40) NOT NULL,
   `inome` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1907 ;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `teste`
---
-
-CREATE TABLE IF NOT EXISTS `teste` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `teste` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tipo`
---
-
-CREATE TABLE IF NOT EXISTS `tipo` (
-  `id_perfil` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nome_perfil` varchar(100) NOT NULL,
-  `ataque_minimo` int(10) unsigned DEFAULT NULL,
-  `defesa_minima` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id_perfil`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
--- Extraindo dados da tabela `tipo`
---
-
-INSERT INTO `tipo` (`id_perfil`, `nome_perfil`, `ataque_minimo`, `defesa_minima`) VALUES
-(1, 'ataca mais  forte', 1, 2),
-(2, 'ataca mais fraco', 1, 1),
-(3, 'ataca continuo alguem', 1, 1),
-(5, 'vingativo', 1, 1),
-(6, 'suicida', 1, 1),
-(7, 'conservador', 1, 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tipos`
---
-
-CREATE TABLE IF NOT EXISTS `tipos` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `descr` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `descr` (`descr`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `ultimo`
---
-
-CREATE TABLE IF NOT EXISTS `ultimo` (
-  `id_atacante` int(11) NOT NULL AUTO_INCREMENT,
-  `id_jogo` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_atacado` int(11) DEFAULT NULL,
-  `numero_de_ataque` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id_atacante`,`id_jogo`),
-  KEY `ultimo_FKIndex1` (`id_atacante`),
-  KEY `ultimo_FKIndex2` (`id_atacado`),
-  KEY `ultimo_FKIndex3` (`id_jogo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `vizinhos`
---
-
-CREATE TABLE IF NOT EXISTS `vizinhos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `territorio` int(11) NOT NULL,
-  `vizinho` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estrutura para visualizar `forca_calculada`
---
-DROP TABLE IF EXISTS `forca_calculada`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `forca_calculada` AS select `forca`.`id_jogo` AS `id_jogo`,`forca`.`id_rodada` AS `id_rodada`,`forca`.`id_jogador` AS `id_jogador`,(((`forca`.`q_continetes` * 1) + (`forca`.`q_paises` * 0.6)) + (`forca`.`q_exercitos` * 0.4)) AS `forca` from `forca` where ((`forca`.`id_jogo` = (select max(`jogo`.`id_jogo`) AS `MAX(jogo.id_jogo)` from `jogo`)) and (`forca`.`id_rodada` = (select max(`rodada`.`id_rodada`) AS `MAX(rodada.id_rodada)` from `rodada` where (`rodada`.`id_jogo` = (select max(`jogo`.`id_jogo`) AS `MAX(jogo.id_jogo)` from `jogo`))))) group by `forca`.`id_jogo`,`forca`.`id_rodada`,`forca`.`id_jogador`;
-
---
--- Restrições para as tabelas dumpadas
---
-
---
--- Restrições para a tabela `jogada`
---
-ALTER TABLE `jogada`
-  ADD CONSTRAINT `jogada_ibfk_2` FOREIGN KEY (`id_rodada`, `id_jogo`) REFERENCES `rodada` (`id_rodada`, `id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restrições para a tabela `jogadores`
---
-ALTER TABLE `jogadores`
-  ADD CONSTRAINT `jogadores_ibfk_1` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restrições para a tabela `perfil`
---
-ALTER TABLE `perfil`
-  ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id_perfil`) REFERENCES `tipo` (`id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `perfil_ibfk_2` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `perfil_ibfk_3` FOREIGN KEY (`id_jogador`) REFERENCES `jogadores` (`id_jogador`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restrições para a tabela `rodada`
---
-ALTER TABLE `rodada`
-  ADD CONSTRAINT `rodada_ibfk_1` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restrições para a tabela `ultimo`
---
-ALTER TABLE `ultimo`
-  ADD CONSTRAINT `ultimo_ibfk_1` FOREIGN KEY (`id_atacante`) REFERENCES `jogadores` (`id_jogador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ultimo_ibfk_2` FOREIGN KEY (`id_atacado`) REFERENCES `jogadores` (`id_jogador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ultimo_ibfk_3` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Extraindo dados da tabela `mapas`
---
-
-INSERT INTO `mapas` (`id`, `nome`, `mapa`, `numero_territorios`, `numero_regioes`, `numero_objetivos`, `tipo`) VALUES
-(83, 'Mapa_final.svg', 'Mapa', 43, NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Extraindo dados da tabela `regioes`
---
-
-INSERT INTO `regioes` (`id`, `id_mapa`, `nome`, `cor`, `exercitos`, `valor_estrategico`) VALUES
-(27, 83, 'America do Sul', 'amarelo', 4, 4),
-(28, 83, 'America do Norte', 'verde', 7, 7),
-(29, 83, 'Europa', 'vermelho', 8, 8),
-(30, 83, 'Africa', 'azul', 5, 5),
-(31, 83, 'Asia', 'branco', 9, 9),
-(32, 83, 'Oceania', 'preto', 4, 4);
 --
 -- Extraindo dados da tabela `territorios`
 --
@@ -458,9 +264,66 @@ INSERT INTO `territorios` (`id`, `id_mapa`, `id_regiao`, `nome`, `numero_vizinho
 (1905, 83, 32, 'novaGuine', NULL, NULL, 'l_novaGuine', 't_novaGuine'),
 (1906, 83, 28, 'cuba', NULL, NULL, 'l_cuba', 't_cuba');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo` (
+  `id_perfil` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome_perfil` varchar(100) NOT NULL,
+  `ataque_minimo` int(10) unsigned DEFAULT NULL,
+  `defesa_minima` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_perfil`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Extraindo dados da tabela `tipo`
+--
+
+INSERT INTO `tipo` (`id_perfil`, `nome_perfil`, `ataque_minimo`, `defesa_minima`) VALUES
+(1, 'ataca mais  forte', 1, 2),
+(2, 'ataca mais fraco', 1, 1),
+(3, 'ataca continuo alguem', 1, 1),
+(5, 'vingativo', 1, 1),
+(6, 'suicida', 1, 1),
+(7, 'conservador', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ultimo`
+--
+
+CREATE TABLE IF NOT EXISTS `ultimo` (
+  `id_atacante` int(11) NOT NULL AUTO_INCREMENT,
+  `id_jogo` int(10) unsigned NOT NULL DEFAULT '0',
+  `id_atacado` int(11) DEFAULT NULL,
+  `numero_de_ataque` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_atacante`,`id_jogo`),
+  KEY `ultimo_FKIndex1` (`id_atacante`),
+  KEY `ultimo_FKIndex2` (`id_atacado`),
+  KEY `ultimo_FKIndex3` (`id_jogo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vizinhos`
+--
+
+CREATE TABLE IF NOT EXISTS `vizinhos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `territorio` int(11) NOT NULL,
+  `vizinho` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=469 ;
+
 --
 -- Extraindo dados da tabela `vizinhos`
 --
+
 INSERT INTO `vizinhos` (`id`, `territorio`, `vizinho`) VALUES
 (301, 1865, 1864),
 (302, 1865, 1866),
@@ -628,6 +491,53 @@ INSERT INTO `vizinhos` (`id`, `territorio`, `vizinho`) VALUES
 (466, 1901, 1904),
 (467, 1901, 1893),
 (468, 1901, 1892);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para visualizar `forca_calculada`
+--
+DROP TABLE IF EXISTS `forca_calculada`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `forca_calculada` AS select `forca`.`id_jogo` AS `id_jogo`,`forca`.`id_rodada` AS `id_rodada`,`forca`.`id_jogador` AS `id_jogador`,(((`forca`.`q_continetes` * 1) + (`forca`.`q_paises` * 0.6)) + (`forca`.`q_exercitos` * 0.4)) AS `forca` from `forca` where ((`forca`.`id_jogo` = (select max(`jogo`.`id_jogo`) AS `MAX(jogo.id_jogo)` from `jogo`)) and (`forca`.`id_rodada` = (select max(`rodada`.`id_rodada`) AS `MAX(rodada.id_rodada)` from `rodada` where (`rodada`.`id_jogo` = (select max(`jogo`.`id_jogo`) AS `MAX(jogo.id_jogo)` from `jogo`))))) group by `forca`.`id_jogo`,`forca`.`id_rodada`,`forca`.`id_jogador`;
+
+--
+-- Restrições para as tabelas dumpadas
+--
+
+--
+-- Restrições para a tabela `jogada`
+--
+ALTER TABLE `jogada`
+  ADD CONSTRAINT `jogada_ibfk_2` FOREIGN KEY (`id_rodada`, `id_jogo`) REFERENCES `rodada` (`id_rodada`, `id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para a tabela `jogadores`
+--
+ALTER TABLE `jogadores`
+  ADD CONSTRAINT `jogadores_ibfk_1` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para a tabela `perfil`
+--
+ALTER TABLE `perfil`
+  ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id_perfil`) REFERENCES `tipo` (`id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `perfil_ibfk_2` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `perfil_ibfk_3` FOREIGN KEY (`id_jogador`) REFERENCES `jogadores` (`id_jogador`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para a tabela `rodada`
+--
+ALTER TABLE `rodada`
+  ADD CONSTRAINT `rodada_ibfk_1` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para a tabela `ultimo`
+--
+ALTER TABLE `ultimo`
+  ADD CONSTRAINT `ultimo_ibfk_1` FOREIGN KEY (`id_atacante`) REFERENCES `jogadores` (`id_jogador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ultimo_ibfk_2` FOREIGN KEY (`id_atacado`) REFERENCES `jogadores` (`id_jogador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ultimo_ibfk_3` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
